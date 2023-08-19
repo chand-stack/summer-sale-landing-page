e35c55f2bd8219056ba3b65d99ebcc20c918029f
chand-stack <chand.stack@gmail.com>
2023-08-20 03:04:13 +0600

    chore: interactive function added

--- script.js
+++ script.js
@@ -5,7 +5,45 @@ function clickItem(target){
     newLi.innerText = target.childNodes[3].childNodes[3].innerText;
     productList.appendChild(newLi);
 
-const productPrice =
+const productPrice = target.childNodes[3].childNodes[5].innerText.split(' ')[0];
+total = total + parseFloat(productPrice);
 
-console.log(target.childNodes[3].childNodes[5].innerText.split(' ')[0])
+const totalPrice = document.getElementById('total-price');
+totalPrice.innerText = total
+
+const purchaseBtn = document.getElementById('purchase-btn');
+
+if(total > 0){
+    purchaseBtn.removeAttribute('disabled')
+}else{
+    purchaseBtn.setAttribute("disabled", "")
 }
+
+const applyBtn = document.getElementById('apply-btn');
+
+if(total>= 200){
+    applyBtn.removeAttribute('disabled');
+}else { applyBtn.setAttribute("disabled", "")}
+
+
+}
+const applyBtn = document.getElementById('apply-btn');
+applyBtn.addEventListener('click', function(){
+
+
+    const inputField = document.getElementById('input')
+    const inputValue = inputField.value;
+    const discountField = document.getElementById('discount')
+    const discountTotal = document.getElementById('total')
+        
+    if(inputValue === 'SELL200'){
+        discountField.innerText = total * 0.2
+        discountTotal.innerText = total - (total*0.2);
+    }else {
+        alert("Invalid Coupon")
+    }
+    
+
+})
+
+
